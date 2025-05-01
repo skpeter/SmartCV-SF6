4ec913cbe7d2f0349addb543276f8f414f768656
Samuel Petersen <samuelkpeter@gmail.com>
2025-05-01 16:38:55 -0300

    put the rivals characters and stages thingies

---
+++ roa2.py
@@ -0,0 +1,31 @@
+characters = [
+  "Fleet",
+  "Zetterburn",
+  "Excaliburn",
+  "Wrastor",
+  "Ranno",
+  "Maypul",
+  "Clairen",
+  "Kragg",
+  "Loxodont",
+  "Orcane",
+  "Forsburn",
+  "Etalus",
+  "Olympia"
+]
+
+stages = [
+  "Aetherian Forest",
+  "Godai Delta",
+  "Hodojo",
+  "Julesvale",
+  "Merchant Port",
+  "Air Armada",
+  "Fire Capital",
+  "Hyperborean Harbor",
+  "Rock Wall",
+  "Tempest Peak",
+  #Doubles
+  "Forest Understory",
+  "Stormswept Pillar",
+]
