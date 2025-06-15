e8c7cf7f247272a8446df1f094b807d55d058f09
Samuel Petersen <samuelkpeter@gmail.com>
2025-06-15 20:25:17 -0300

    probably the most accurate health detection will get

--- routines.py
+++ routines.py
@@ -56,13 +56,12 @@ def detect_characters(payload:dict, img, scale_x:float, scale_y:float):
     x,y,w,h = (int(100 * scale_x), int(915 * scale_y), int(1880 * scale_x), int(90 * scale_y))
     img = np.array(img)
     cropped = img[int(y):int(y+h), int(x):int(x+w)]
-    # signal to the main loop that character and tag detection is in progress
     if payload['state'] != "loading" or payload['players'][0]['character']: return
     try:
         stitched = core.stitch_text_regions(cropped, int(80 * scale_y), (255,255,255), margin=30, deviation=0.4) 
         cropped = stitched.copy()
         characters = core.read_text(cropped, contrast=2)
-        if characters: characters = characters.lower().replace('chun li', 'chun-li').split(' ')
+        if characters: characters = characters.replace(' li', '-li').split('|')
     except:
         pass
     if 'stitched' not in locals() or len(stitched) != 2:
@@ -74,8 +73,8 @@ def detect_characters(payload:dict, img, scale_x:float, scale_y:float):
         x,y,w,h = (int(105 * scale_x), int(820 * scale_y), int(1713 * scale_x), int(127 * scale_y))
         cropped = img[int(y):int(y+h), int(x):int(x+w)]
         characters = core.read_text(cropped, contrast=2)
-        if characters: characters = characters.lower().replace('chun li', 'chun-li').split(' ')
-        if characters and len(characters) == 4:
+        if characters: characters = characters.replace(' li', '-li').split('|')
+        if characters and len(characters) >= 4:
             c1, _ = findBestMatch(characters[0], sf6.characters)
             c2, _ = findBestMatch(characters[1], sf6.characters)
             t1 = characters[2]
@@ -88,7 +87,7 @@ def detect_characters(payload:dict, img, scale_x:float, scale_y:float):
 def detect_versus_screen(payload:dict, img, scale_x:float, scale_y:float):
     
     box = (int(928 * scale_x), int(50 * scale_y), int(26 * scale_x), int(187 * scale_y))
-    if core.get_color_match_in_region(img, box, (34, 7, 9), 0.1) >= 0.9:
+    if core.get_color_match_in_region(img, box, (34, 7, 9), 0.05) >= 0.9:
         payload['state'] = "loading"
         if payload['state'] != previous_states[-1]:
             previous_states.append(payload['state'])
@@ -116,22 +115,25 @@ def detect_round_start(payload:dict, img, scale_x:float, scale_y:float):
         if payload['state'] != previous_states[-1]:
             previous_states.append(payload['state'])
 
-def detect_health_bars(payload:dict, img, scale_x:float, scale_y:float, detect_ko=False):
-    if not detect_ko and (payload['players'][0]['health'] == 0 or payload['players'][1]['health'] == 0): return
+ko_passes = 0
+def detect_health_bars(payload:dict, img, scale_x:float, scale_y:float):
+    global ko_passes
+    if ko_passes < 1 and (payload['players'][0]['health'] == 0 or payload['players'][1]['health'] == 0): return
     img = img.convert("L")
     enhancer = ImageEnhance.Contrast(img)
-    img = enhancer.enhance(3.0)  # adjust the factor as needed
+    img = enhancer.enhance(3)  # adjust the factor as needed
     img.save('temp.png')
-    healthbar1 = core.get_color_match_in_region(img, (int(167 * scale_x), int(63 * scale_y), int(680 * scale_x), 1), (210, 210, 210), 0.2)
-    healthbar2 = core.get_color_match_in_region(img, (int(1071 * scale_x), int(63 * scale_y), int(680 * scale_x), 1), (210, 210, 210), 0.2)
+    healthbar1 = core.get_color_match_in_region(img, (int(167 * scale_x), int(63 * scale_y), int(682 * scale_x), 1), (230, 230, 230), 0.175)
+    healthbar2 = core.get_color_match_in_region(img, (int(1070 * scale_x), int(63 * scale_y), int(682 * scale_x), 1), (230, 230, 230), 0.175)
     payload['players'][0]['health'] = int(healthbar1 * 100)
     payload['players'][1]['health'] = int(healthbar2 * 100)
 
     if config.getboolean('settings', 'debug_mode', fallback=False):
-        print("Player 1 health bar:", healthbar1, "Player 2 health bar:", healthbar2, "KO Check:", detect_ko)
+        print("Player 1 health bar:", healthbar1, "Player 2 health bar:", healthbar2, "KO Check:", ko_passes)
     
-    if np.floor(healthbar1 * 100) / 100 == 0 or np.floor(healthbar2 * 100) / 100 == 0:
-        if detect_ko:
+    if (np.floor(healthbar1 * 100) / 100 == 0) ^ (np.floor(healthbar2 * 100) / 100 == 0):
+        ko_passes += 1
+        if ko_passes > 3:
             core.print_with_time("- K.O.", end=" ")
             if healthbar1 < healthbar2:
                 payload['players'][0]['rounds'] -= 1
@@ -140,8 +142,8 @@ def detect_health_bars(payload:dict, img, scale_x:float, scale_y:float, detect_k
                 payload['players'][1]['rounds'] -= 1
                 print(f"by {payload['players'][0]['character']}")
             else:
-                time.sleep(0.5)
-                return # detect_health_bars(payload, img, scale_x, scale_y, detect_ko=True)
+                return # detect_health_bars(payload, img, scale_x, scale_y)
+            ko_passes = 0
             payload['round'] += 1
             if payload['players'][0]['rounds'] == 0 or payload['players'][1]['rounds'] == 0:
                 core.print_with_time(f"{payload['players'][0]['character'] if payload['players'][0]['rounds'] > 0 else payload['players'][1]['character']} wins!")
@@ -149,10 +151,8 @@ def detect_health_bars(payload:dict, img, scale_x:float, scale_y:float, detect_k
                 payload['round'] = 1
                 if payload['state'] != previous_states[-1]:
                     previous_states.append(payload['state'])
-            return
-        time.sleep(1.25)
-        return detect_health_bars(payload, img, scale_x, scale_y, detect_ko=True)
-            
+        return
+    else: ko_passes -= 1 if ko_passes > 0 else 0
     return
 
 def detect_results(payload:dict, img, scale_x:float, scale_y:float):
